a63504634e3a040bc53bc6e514571142cc4c9195
Himanshu Choudhary <himanshu@wimwisure.com>
2021-01-28 16:13:57 +0530

    Updated view extensions

--- customktx/src/main/java/com/himanshu/customktx/extensions/view/ViewExtensions.kt
+++ customktx/src/main/java/com/himanshu/customktx/extensions/view/ViewExtensions.kt
@@ -1,8 +1,10 @@
 package com.himanshu.customktx.extensions.view
 
 import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.IdRes
 import com.google.android.material.snackbar.Snackbar
 
@@ -22,7 +24,31 @@ fun Activity.findAndSetTextInTextView(@IdRes id: Int, text: String) {
     findViewById<TextView>(id).text = text
 }
 
-fun View.showTextInSnackbar(text: String) {
+/**
+ * Toast extensions.
+ */
+
+fun Context.showSmallLengthToast(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showLongLengthToast(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+}
+
+/**
+ * Snackbar extensions.
+ */
+
+fun View.showSmallSnackbar(text: String) {
+    Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
+}
+
+fun View.showLongSnackbar(text: String) {
     Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
+}
+
+fun View.showIndefiniteSnackbar(text: String) {
+    Snackbar.make(this, text, Snackbar.LENGTH_INDEFINITE).show()
 }
 
--- docs/ActivityExtension.md
+++ docs/ActivityExtension.md
@@ -1,3 +1,5 @@
 Here, we will be talking about extension which are particularly related to the `Activity`.
 
 1. Intents
+
+
